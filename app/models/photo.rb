21c7b56b6afed2fc7aac7b2caccfd862db386baa
Jay D <jduffy@users.noreply.github.com>
2016-11-22 19:54:39 -0600

    jjduffy333_pgauth_3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,11 +1,9 @@
 class Photo < ActiveRecord::Base
 
-validates :photo_id, :presence => true
+validates :user_id, :presence => true
 belongs_to :user
 has_many :comments
 has_many :likes
 has_many :fans, :through => :likes, :source => :user
-
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   # In config/routes.rb
   root "photos#index"
-  
+
+  #Routs for the User list
+  get "/users", :controller => "users", :action => "index"
+
   # Routes for the Comment resource:
   # CREATE
   get "/comments/new", :controller => "comments", :action => "new"
